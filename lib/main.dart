a4c72aa315006515002c8d82886c8f97f57a946b
arridhaamrad <arridhaamrad@gmail.com>
2021-03-07 14:04:59 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,95 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<bool> _selections = [false, true, false];
 
-  void _incrementCounter() {
+  void updateSelection(int selectedIndex) {
     setState(() {
-      _counter++;
+      for (var i = 0; i < _selections.length; i++) {
+        _selections[i] = selectedIndex == i;
+      }
+    });
+  }
+
+  String tip;
+
+  final _controller = TextEditingController();
+
+  void calculateTip() {
+    if (_controller.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Please input your ammount"),
+      ));
+      return;
+    }
+    var amount = double.tryParse(_controller.text);
+    var selectedIndex = _selections.indexWhere((element) => element);
+    var percentage = [0.1, 0.15, 0.2][selectedIndex];
+
+    String tipAmount = (amount * percentage).toStringAsFixed(2);
+
+    setState(() {
+      tip = "\$$tipAmount";
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Text(
-              'You have pushed the button this many times:',
+              (tip ?? "0"),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Text("Total Amount"),
+            SizedBox(
+              width: 80,
+              child: TextField(
+                controller: _controller,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: "\$100",
+                    hintStyle: TextStyle(color: Colors.grey[400])),
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: ToggleButtons(
+                  children: [Text('10%'), Text('15%'), Text('20%')],
+                  isSelected: _selections,
+                  onPressed: updateSelection),
+            ),
+            ElevatedButton(
+                style: ButtonStyle(elevation: MaterialStateProperty.all(0.0)),
+                onPressed: calculateTip,
+                child: Text("Calculate Tip"))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
